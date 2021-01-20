21b9c0b4e2d3464180a2784ffb984d35a3c7fbe6
Aditya Bhadange <aditya.bhadange18@gmail.com>
2021-01-20 11:52:59 +0530

    Added product app in INSTALLED APPS

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')
 INSTALLED_APPS = [
     'app',  # Enable the inner app
     'import_export',
-
+    'product',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
