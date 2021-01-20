77b29bf9991cccd402cdcb9c2ef76b82ace5e526
Aditya Bhadange <aditya.bhadange18@gmail.com>
2021-01-20 13:10:37 +0530

    Added search bar UI

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     # The home page
     path('', views.index, name='home'),
 
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
+    # # Matches any html file
+    # re_path(r'^.*\.*', views.pages, name='pages'),
 
 ]
