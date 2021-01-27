b3f83f620637d34fa6186aabc2103d741be40609
Aditya Bhadange <aditya.bhadange18@gmail.com>
2021-01-27 15:28:49 +0530

    changed urls.py and deleted some files

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,6 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 from django.urls import path, include  # add this
 from product import views
-# from orders import views as orders_views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
